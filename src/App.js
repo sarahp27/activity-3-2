fdd497693edfdebbd7e1fa431c551f4114e5b900
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-19 19:32:18 -0700

    blog post remaining

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from 'react';
 import BlogPosts from './components/BlogPosts';
 import NewPost from './components/NewPost';
 
 function App() {
-  // Todo: Fetch blog posts from https://jsonplaceholder.typicode.com/posts (see documentation on https://jsonplaceholder.typicode.com/guide/)
-  // Pass fetched posts to <BlogPost /> via props & output the posts in that component
+  const [post, setPost] = useState([]);
+  const [waiting, setWaiting ] = useState(false);
 
+  useEffect(()=> {
+    setWaiting(true)
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((respond) =>{
+      return respond.json();
+    })
+    .then(post =>{
+      setPost(post.slice(0,3))
+      setWaiting(false)
+      console.log(post.slice(0,3))
+    })
+  },[])
+  
   return (
     <>
-      <NewPost />
-      <BlogPosts />
+
+      <NewPost setPost={setPost} post={post} waiting={waiting} setWaiting={setWaiting}/>
+      <BlogPosts  />
     </>
   );
 }
